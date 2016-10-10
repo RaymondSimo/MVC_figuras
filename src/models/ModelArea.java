92a073f521b61300965c2dc30e7b8ee1187ce94e
diego hernandez <diego hernandez>
2016-10-10 18:24:59 -0500

    Area y libreria

---
+++ src/models/ModelArea.java
@@ -0,0 +1,71 @@
+/*
+Implementacion del modelo Area
+ */
+package models;
+
+/**
+ *
+ * @author DDHL97
+ */
+public class ModelArea {
+    private double resultadoa;
+    private double altura;
+    private double radio;
+    
+    
+    public void cilindro(){
+        setResultadoa(((2*3.1416) * getRadio()) * (getAltura() + getRadio()));
+    }
+    public void cubo(){
+        double areaCara = getAltura()*getAltura();
+        setResultadoa(6*(areaCara*areaCara));
+    }
+    public void esfera(){
+        setResultadoa((4*3.1416) * (getRadio() * getRadio()));
+        
+    }
+
+    /**
+     * @return the resultadoa
+     */
+    public double getResultadoa() {
+        return resultadoa;
+    }
+
+    /**
+     * @param resultadoa the resultadoa to set
+     */
+    public void setResultadoa(double resultadoa) {
+        this.resultadoa = resultadoa;
+    }
+
+    /**
+     * @return the altura
+     */
+    public double getAltura() {
+        return altura;
+    }
+
+    /**
+     * @param altura the altura to set
+     */
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    /**
+     * @return the radio
+     */
+    public double getRadio() {
+        return radio;
+    }
+
+    /**
+     * @param radio the radio to set
+     */
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+  
+}
